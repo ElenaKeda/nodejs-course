58906c0c3363121a89c79c5c14ec19f704ebd29a
Elena Keda <elena_keda_92@mail.ru>
2026-04-27 21:38:06 +0300

    fixed shop and admin controllers and product model

--- models/product.js
+++ models/product.js
@@ -1,29 +1,70 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
 
-const sequelize = require("../util/database");
+const getDb = require("../util/database").getDb;
 
-const Product = sequelize.define("product", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  title: {
-    type: Sequelize.STRING,
-  },
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false,
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-});
+class Product {
+  constructor({ title, price, description, imageUrl, id }) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+    this._id = id ? new mongodb.ObjectId(id) : null;
+  }
+
+  save() {
+    const db = getDb();
+    let dbOp;
+
+    if (this._id) {
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      dbOp = db.collection("products").insertOne(this);
+    }
+    return dbOp
+      .then(() => {
+        console.log('Saved!');
+      })
+      .catch((err) => console.log({ saveDbErr: err }));
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((products) => {
+        console.log({ products });
+        return products;
+      })
+      .catch((err) => console.log({ fetchAllDbErr: err }));
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then((product) => {
+        console.log({ product });
+        return product;
+      })
+      .catch((err) => console.log({ findByIdDbErr: err }));
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then(() => {
+        console.log('Deleted!');
+      })
+      .catch((err) => console.log({ findByIdDbErr: err }));
+  }
+}
 
 module.exports = Product;
