fa8a84d4a6643fa1d138efb5146198e106e6b4ff
Elena Keda <elena_keda_92@mail.ru>
2026-04-27 19:25:46 +0300

    rewrote database file

--- util/database.js
+++ util/database.js
@@ -1,8 +1,30 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
 
-const sequelize = new Sequelize("nodejs-course", "root", "6097278", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb://elenakedamail_db_user:<password>@ac-nbsuhc2-shard-00-00.ixpnbhi.mongodb.net:27017,ac-nbsuhc2-shard-00-01.ixpnbhi.mongodb.net:27017,ac-nbsuhc2-shard-00-02.ixpnbhi.mongodb.net:27017/?ssl=true&replicaSet=atlas-tdt8x2-shard-0&authSource=admin&appName=Cluster0",
+  )
+    .then((client) => {
+      console.log("Connected!");
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log({ mondoDbConnectionErr: err });
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
